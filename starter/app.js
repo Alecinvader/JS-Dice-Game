d90ba232b941c8e9fabaf231806d61d76b5a4dd6
alec-r-reynolds <alechasrito@gmail.com>
2020-07-28 22:25:37 -0600

    Dice change and round score

--- starter/app.js
+++ starter/app.js
@@ -9,21 +9,45 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1; 
-
-document.querySelector('#current-' + activePlayer).textContent = dice; 
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  
+  // 1. Random Number
+  var dice = Math.floor(Math.random() * 6) + 1; 
+
+  // 2. Display result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score if the rolled number was not a 1 
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore; 
+  } else {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.querySelector('#current-0').textContent = '0'; 
+    document.querySelector('#current-0').textContent = '0'; 
+
+    
+  }
+ 
+ 
+});
 
 
 
