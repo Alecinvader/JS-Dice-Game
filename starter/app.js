a001435c40bfc49db8c09ac5960dc66a04b4eb9c
alec-r-reynolds <alechasrito@gmail.com>
2020-07-28 23:15:33 -0600

    Init Handler

--- starter/app.js
+++ starter/app.js
@@ -11,9 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 
 document.querySelector('.dice').style.display = 'none';
@@ -22,6 +20,8 @@ document.getElementById('score-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
 document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
+
+document.querySelector('btn-new').addEventListener('click', init);
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   
@@ -51,8 +51,15 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
   
   // Check if player won game
-  if (scores[activePlayer] >= 100) {
+  if (scores[activePlayer] >= 10) {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    
+   
+    
+
   } else {
     nextPlayer();
   }
@@ -73,6 +80,29 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none'; 
+}
+
+
+function init() {
+  scores = [0,0];
+  activePlayer = 0;
+  roundScore = 0;
+  
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.add('active');
+
+  
 }
 
 
